64828b35ec8f3a44da47ac292bf50d72fa2f0ea6
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-17 10:56:57 -0500

    Using generator functions to countDown, make a On Off Switch, and Prevent Infinite Loops

--- scripts.js
+++ scripts.js
@@ -5,15 +5,100 @@ console.log('test');
 // }
 
 // console.log(clickMe());
+function logging(){
+    console.log('hi');
+}
 
-function* clickMeMore(){
+function* clickMeMore(num1, num2){
     yield "I've been clicked";
     yield "I've been clicked, again!";
     yield "AND AGAIN"
+    yield logging();
+    yield num1 > num2 ?'yes' : 'no'
+    yield 12;
+    yield numberArray = [1,2,3,4,5];
+    yield numberArray[2];
+    yield dataObject = {
+        state: 'state'
+    }
+    yield dataObject.state;    
+}
+const clickClack = clickMeMore(5, 2);
+// const clickClack = clickMeMore();
+
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+// console.log(clickClack.next());
+
+function* lightSwitch(){
+   let container = 'on'
+   let container2 = 'off'
+    while(true){
+        yield container;
+        yield container2;
+    }
 }
 
-const clickClack = clickMeMore();
+const humanSwitch = lightSwitch();
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next()); console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next()); console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
+console.log(humanSwitch.next());
 
-console.log(clickClack.next());
-console.log(clickClack.next());
-console.log(clickClack.next());
+function* countDown(){
+    let a = 10;
+    while(a > 0){
+        yield `Lunching in ${a}`;
+        a -= 1;
+    }
+    yield 'Take off!!!!!!!!!!!!!!!!!!!!!!';
+}
+
+const takeOff = countDown();
+
+
+console.log(takeOff.next());
+console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next()); console.log(takeOff.next());
